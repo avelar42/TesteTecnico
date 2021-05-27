90585a3f0a95f9ab6443ff6cf2e11e6bea0367ae
avelar42 <avelar42wow@gmail.com>
2021-05-26 22:16:23 -0300

    Add project files.

---
+++ TesteTecnico/Controllers/CalculaJurosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteTecnico.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculaJurosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult("CalculadoraJuros");
+        }
+    }
+}
---
+++ TesteTecnico/Controllers/TaxaJurosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteTecnico.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaJurosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string result = "0,01";
+            return new JsonResult(result);
+        }
+    }
+}
