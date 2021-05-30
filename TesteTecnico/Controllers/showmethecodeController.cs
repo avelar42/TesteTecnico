5d6492d6b9c30b2b1253496b65732f07bb2459c0
avelar42 <avelar42wow@gmail.com>
2021-05-30 10:33:23 -0300

    -IMplementação docker e endpoint ShowMeTheCode

---
+++ TesteTecnico/Controllers/showmethecodeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteTecnico.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class showmethecodeController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var url = "https://github.com/avelar42/TesteTecnico";
+            return new JsonResult(url);
+        }
+    }
+}
